56e1b4e1ff9b66e3973468fdc22a270a24fff7c3
jack90nguyen <jack90nguyen@gmail.com>
2023-04-04 16:02:30 +0700

    Tong quan OKRs dang treeview

--- OnetezSoft/wwwroot/js/app.js
+++ OnetezSoft/wwwroot/js/app.js
@@ -120,6 +120,52 @@ function scrollGantt(value) {
   }, 500);
 }
 
+function dragScroll(elementId) {
+  setTimeout(function () {
+    const element = document.getElementById(elementId);
+    element.style.cursor = "grab";
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+    const mouseDownHandler = function (e) {
+      element.style.cursor = "grabbing";
+      element.style.userSelect = "none";
+
+      pos = {
+        left: element.scrollLeft,
+        top: element.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+      };
+
+      document.addEventListener("mousemove", mouseMoveHandler);
+      document.addEventListener("mouseup", mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+      // How far the mouse has been moved
+      const dx = e.clientX - pos.x;
+      const dy = e.clientY - pos.y;
+
+      // Scroll the element
+      element.scrollTop = pos.top - dy;
+      element.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function () {
+      element.style.cursor = "grab";
+      element.style.removeProperty("user-select");
+
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      document.removeEventListener("mouseup", mouseUpHandler);
+    };
+
+    // Attach the handler
+    element.addEventListener("mousedown", mouseDownHandler);
+  }, 500);
+}
+
 function toggleText(item) {
   if (item.className.indexOf("is-show") === -1) item.classList.add("is-show");
   else item.classList.remove("is-show");
