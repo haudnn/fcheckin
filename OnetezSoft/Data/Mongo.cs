df58c98fec0d32f65a69c432e9ebcb1f0cdeaf23
jack90nguyen <jack90nguyen@gmail.com>
2023-05-08 11:53:41 +0700

    Using MongoDB Atlas

--- OnetezSoft/Data/Mongo.cs
+++ OnetezSoft/Data/Mongo.cs
@@ -6,10 +6,13 @@ namespace OnetezSoft.Data
 {
   public class Mongo
   {
+    private static readonly string ConnectionString =
+      "mongodb+srv://conando_test:ueczFaZcPqvMYdsv@conandotest.eei9mbf.mongodb.net/?retryWrites=true&w=majority";
+
     public static IMongoDatabase DbConnect(string name)
     {
-      //var client = new MongoClient(ConnectionString);
-      var client = new MongoClient();
+      //var client = new MongoClient();
+      var client = new MongoClient(ConnectionString);
       return client.GetDatabase(name);
     }
 
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Fastdo
+# FASTDO
 
 FASTDO is a web app: OKRs, Todolist, Kaizen, Educate, Plan
+
+# ACCOUNT
+
+- Username: dev.conando@gmail.com
+
+- Password: 123456@#"
+
+> Developed by jack90nguyen
